5fef72d235265e6eae7bea4e8c08655f9170234a
Brady Burgener <bradylbsee@gmail.com>
2025-03-01 12:41:28 -0700

    adds links

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 console.log("Section 1");
 let ages = [3,9,23,64,2,8,28,93]; //initial array given
 
-console.log("1. An array called ages starts with a set array.  Find the difference between the first and last element programmatically.");
+console.log("1. Programmatically subtract the value of the first element in the array from the value in the last element of the array. Do not use numbers to reference the last element, find it programmatically.");
 // function created to determine the difference between the first and last age.
 function differenceAge(ages){
     console.log(ages); //prints array to the console
@@ -10,11 +10,11 @@ function differenceAge(ages){
 }
 differenceAge(ages);//calls the function differenceAge
 
-console.log("2. Add a new age to the end of the array and repeat.");
+console.log("2. Add a new age to your array and repeat the step above to ensure it is dynamic. (works for arrays of different lengths).");
 ages.push(50);//adds 50 to the end of the array ages
 differenceAge(ages);//calls the function differenceAge again
 
-console.log("3. Use a loop to iterated through the array to find the average age.")
+console.log("3. Use a loop to iterate through the array and calculate the average age.")
 let averageAge = 0;// set averageAge to 0
 //A loop to add all the ages in ages to averageAge
 for(let i=0; i< ages.length; i++){
@@ -28,7 +28,7 @@ let names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'];
 console.log ("Starting with an array called names:");
 console.log(names);
 
-console.log("1. Using a loop calculate the average letters per name:");
+console.log("1. Use a loop to iterate through the array and calculate the average number of letters per name.");
 let averageNameLength = 0;
 for(nameIn of names){
     averageNameLength += nameIn.length;
@@ -36,7 +36,7 @@ for(nameIn of names){
 averageNameLength = averageNameLength/names.length;
 console.log(averageNameLength);
 
-console.log("2. Using a loop concatenate all the names together:");
+console.log("2. Use a loop to iterate through the array again and concatenate all the names together, separated by spaces.");
 let nameString = "";
 for(let i=0; i< names.length; i++){
     nameString += names[i];
@@ -49,3 +49,12 @@ console.log(nameString);
 console.log("using join()")
 nameString = names.join(" ");
 console.log(nameString);
+
+console.log("3. How do you access the last element of any array?");
+console.log("");
+
+console.log("4. How do you access the first element of any array?");
+console.log("");
+
+console.log("5. Create a new array called nameLengths. Write a loop to iterate over the previously created names array and add the length of each name to the nameLengths array.");
+
